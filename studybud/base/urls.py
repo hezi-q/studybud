a94ed7eee26c2763b7dc3ed78d990e7e79990931
Muhammad Hamza Qureshi <hezi.qureshi@gmail.com>
2025-08-24 13:20:43 +0500

    Chat room CRD && divided templates into components

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -7,10 +7,14 @@ urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_page, name='logout'),
     path('register/', views.register_page, name='register'),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
 
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>', views.update_room, name='update-room'),
     path('delete-room/<str:pk>', views.delete_room, name='delete-room'),
+
+    path('delete-message/<str:pk>', views.delete_message, name='delete-message'),
+
 ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 
 from .forms import RoomForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 
 
 def home(request):
@@ -21,17 +21,38 @@ def home(request):
         Q(host__username__icontains=q)
     )
 
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
     context = {
         'rooms': rooms,
         'room_count': rooms.count(),
-        'topics': Topic.objects.all()
+        'topics': Topic.objects.all(),
+        'room_messages': room_messages
     }
 
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    context = {'room': Room.objects.get(id=pk)}
+    room = Room.objects.get(id=pk)
+    messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = room.message_set.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+
+        room.participants.add(request.user)
+
+        return redirect('room', pk=room.id)
+    
+        
+
+    context = {'room': Room.objects.get(id=pk), 'room_messages': messages, 'participants': participants}
+
     return render(request, 'base/room.html', context)
 
 
@@ -69,6 +90,12 @@ def update_room(request, pk):
     return render(request, 'base/room_form.html', context)
 
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    context = {'user': user}
+    return render(request, 'base/profile.html', context)
+
+
 @login_required(login_url='login')
 def delete_room(request, pk):
     room = Room.objects.get(id=pk)
@@ -90,8 +117,6 @@ def login_page(request):
     if request.method == 'POST':
         username = request.POST.get('username').lower()
         password = request.POST.get('password')
-
-        print(username, password)
 
         try:
             user = User.objects.get(username=username)
@@ -136,3 +161,15 @@ def register_page(request):
     return render(request, 'base/login_register.html', context)
 
 
+@login_required(login_url='login')
+def delete_message(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!')
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    
+    return render(request, 'base/delete.html', {'obj': message})
